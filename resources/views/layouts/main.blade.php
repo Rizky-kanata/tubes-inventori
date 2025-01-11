1fe882b7cf32b773ac0efc2526db5397a622ff74
Narendra Darel <narendra.darelrafi@gmail.com>
2025-01-11 23:03:13 +0700

    Login dan Dashboard

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aplikasi Inventaris Sederhana</title>
+    <title>Warung Madura</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
